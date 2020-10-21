1c5965357f176d54930dfaf6518771f9cda97587
Khaled ElHadary <khaledelhadary@outlook.com>
2020-10-21 19:06:30 +0200

    Removed duplicates from pubspec.yaml

--- monqez_app/lib/Screens/HomeScreenMap.dart
+++ monqez_app/lib/Screens/HomeScreenMap.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'HomeScreen.dart';
 
 class HomeScreenMap extends StatefulWidget {
   @override
