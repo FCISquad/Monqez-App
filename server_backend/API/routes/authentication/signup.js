0c94338f02346aab280a76c19eb3ff3fab6b4b46
Ehab Fawzy Ibrahim <ef.ehabfawzy@gmail.com>
2020-11-02 23:48:03 +0200

    Revert "Revert "refactor signup function""
    This reverts commit 631ce6689eedaca08dcbbed4f56f26454e1a8ed7.

--- server_backend/API/routes/authentication/signup.js
+++ server_backend/API/routes/authentication/signup.js
@@ -1,60 +1,32 @@
-const express = require('express');
-const router = express.Router();
-const admin = require('firebase-admin');
-const qs = require('querystring');
+const express = require('express')
+const router = express.Router()
 
-router.post('/' , function (request,response){
-    var body = '';
-    request.on('data', function (data) {
-        body += data;
-        // Too much POST data, kill the connection!
-        // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
-        if (body.length > 1e6)
-            request.connection.destroy();
-    });
-
-    var obj;
-    request.on('end', function () {
-        obj = JSON.parse(body);
-        console.log(obj.token);
-        console.log(obj.uid);
-        console.log(obj.name);
-        console.log(obj.national_id);
-        console.log(obj.phone);
+const http = require('http')
+const helper = require('../../helper/RequestFunctions')
+const database = require('../../firebase/databaseInterface')
 
 
-        admin.auth().verifyIdToken(obj.token).then(function(decodedToken){
-            var uid = decodedToken.uid;
-            if ( uid === obj.uid ){
-                admin.database().ref('users/' + uid).set({
-                    name: obj.name,
-                    phone: obj.phone,
-                    nationalID: obj.national_id
-                })
+router.post('/' , function(request , response){
+    helper.RequestToJson(request , function (requestJson){
+        if ( requestJson === null ){
+            response.send(http.STATUS_CODES[400])
+        }
+        else{
+            database.createUser(requestJson , function (status){
+                if ( status === 'unauthorized' ){
+                    response.send(http.STATUS_CODES[401])
+                }
+                else if ( status == 'invalid id' ){
+                    response.send(http.STATUS_CODES[403])
+                }
+                else if ( status == 'okay' ){
+                    response.send(http.STATUS_CODES[200])
+                }
+            })
+        }
+    })
+})
 
-                // response
-                response.status(200).json({
-                    "message": "okay"
-                });
-            }
-            else{
-                response.status(404);
-                response.status(404).json({
-                    "message" : "hatem"
-                });
-            }
-            console.log(uid);
-            console.log(obj.uid);
-        }).catch(function (error){
-            console.log(error);
-            response.status(404).json({
-               "message" : "hatem"
-            });
-        });
-    });
-
-
-});
 
 
 module.exports = router;
