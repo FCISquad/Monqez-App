631ce6689eedaca08dcbbed4f56f26454e1a8ed7
Ehab Fawzy Ibrahim <ef.ehabfawzy@gmail.com>
2020-11-02 23:46:54 +0200

    Revert "refactor signup function"
    This reverts commit 2b6d87653d87a0677a107942f8570458ffad38fc.

--- server_backend/Monqez.js
+++ server_backend/Monqez.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 
+var bodyParser = require('body-parser');
+// app.use(bodyParser.json);
+app.use(bodyParser.json({ type: 'application/*+json' }))
+
+
 /*
     Firebase database connection
  */
