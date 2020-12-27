7280f9111feefa5d001649819b06ee56cbb4728d
= <ehab1571999@yahoo.com>
2020-12-27 15:45:56 +0200

    Update monqez.js

---
+++ server_backend/monqez.js
@@ -0,0 +1,29 @@
+const bodyParser = require('body-parser');
+const session = require("express-session");
+const express = require('express');
+const app = express();
+
+const admin = require('firebase-admin');
+const serviceAccount = require('./firebase-Monqez.json');
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://monqez-6f9b1.firebaseio.com"
+});
+
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+
+
+const signupRoute = require('./API/Controller/User/userController');
+app.use('/user' , signupRoute);
+
+
+const path = require('./api/code');
+app.use('/code' , path);
+
+module.exports = app;
