99b247b9e1d43846d2d04ab11664599e485a9161
Khaled Elhadary <Khaledelhadary@outlook.com>
2021-05-23 20:43:03 +0200

    Deployment update

--- server_backend/server.js
+++ server_backend/server.js
@@ -14,14 +14,14 @@ const port = process.env.PORT || '5000';
 app.listen(port, () => console.log(`Server started on Port ${port}`));
 
 
-(async () => {
-    const tunnel = await localtunnel({ port: 5000, subdomain: "monqez" });
-
-    // the assigned public url for your tunnel
-    // i.e. https://abcdefgjhij.localtunnel.me
-    console.log(tunnel.url);
-
-    tunnel.on('close', () => {
-        // tunnels are closed
-    });
-})();
+// (async () => {
+//     const tunnel = await localtunnel({ port: 5000, subdomain: "monqez" });
+//
+//     // the assigned public url for your tunnel
+//     // i.e. https://abcdefgjhij.localtunnel.me
+//     console.log(tunnel.url);
+//
+//     tunnel.on('close', () => {
+//         // tunnels are closed
+//     });
+//})();
