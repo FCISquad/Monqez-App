726f2fb18416d1d74005ec1846bfb1aeea579cc9
= <ehab1571999@yahoo.com>
2021-07-11 00:51:54 +0200

    update

--- server_backend/monqez.js
+++ server_backend/monqez.js
@@ -19,9 +19,9 @@ app.use(bodyParser.urlencoded({parameterLimit: 100000,limit: '100mb', extended :
 app.use(bodyParser.json({limit: '100mb'}));
 
 
-// const userRoute = require('./API/Controller/User/userController');
-// app.use('/user' , userRoute);
-//
+const userRoute = require('./API/Controller/User/userController');
+app.use('/user' , userRoute);
+
 // const adminRoute = require('./API/Controller/User/adminController');
 // app.use('/admin' , adminRoute);
 //
